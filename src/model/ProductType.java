4cb0bccfc71c63828363cf01dc0fe19a7220956d
CeceLOursMielleux <etu49139@henallux.be>
2023-04-20 10:28:41 +0200

    commit du labo du 20/04/2023 pour les setters

--- src/model/ProductType.java
+++ src/model/ProductType.java
@@ -1,4 +1,17 @@
 package model;
 
+import java.security.ProtectionDomain;
+
 public class ProductType {
+    private int reference; // foreign key
+    private String label;
+
+    public ProductType(int reference, String label){
+        setReference(reference);
+        this.label = label;
+    }
+
+    public void setReference(int reference) {
+        // c'est censé être un auto incrémentation
+    }
 }
---
+++ src/model/Recherche3.java
@@ -0,0 +1,4 @@
+package model;
+
+public class Recherche3 {
+}
