36bc72d019b67421618477e13f8a3d49fa9ee17b
StevauxLoic <etu48693@henallux.be>
2023-04-20 10:30:17 +0200

    modofications mineurs dans MainWindow.java et UserManuelWindow.java

--- src/userInterface/MainWindow.java
+++ src/userInterface/MainWindow.java
@@ -1,4 +1,37 @@
 package userInterface;
 
-public class MainWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    JMenuBar menuBar;
+    JMenu fileMenu, helpMenu, searchMenu, productMenu;
+    JMenuItem exitMenuItem,
+            userManuelMenuItem,
+            softwareInfosMenuItem,
+            searchProductMenuItem,
+            searchAProductQuantityMenuItem,
+            searchProductSupplementsDueToEventMenuItem,
+            searchAdressMenuItem,
+            searchOutOfStockProductMenuItem,
+            newProductMenuItem,
+            deleteProductMenuItem,
+            modifyProductMenuItem,
+            findProductMenuItem;
+    JPanel welcomePanel;
+
+    public MainWindow() throws HeadlessException {
+        super("Application de gestion");
+        setBounds(100, 100, 500, 500);
+        this.setLayout(new FlowLayout());
+
+        menuBar = new JMenuBar();
+
+        fileMenu = new JMenu("fichier");
+        helpMenu = new JMenu("aide");
+        searchMenu = new JMenu("recherche");
+        productMenu = new JMenu("produit");
+
+
+    }
 }
