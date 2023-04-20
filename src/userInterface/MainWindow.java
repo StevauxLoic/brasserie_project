94d8ea1eb0ecf1190ec508b3f586752df4a4e85d
StevauxLoic <etu48693@henallux.be>
2023-04-20 11:12:02 +0200

    création de la MainWindow.java et du main dans Main.java et ajout de la fenêtre de base

--- src/userInterface/MainWindow.java
+++ src/userInterface/MainWindow.java
@@ -19,19 +19,56 @@ public class MainWindow extends JFrame {
             modifyProductMenuItem,
             findProductMenuItem;
     JPanel welcomePanel;
+    JLabel welcomeLabel;
 
     public MainWindow() throws HeadlessException {
         super("Application de gestion");
         setBounds(100, 100, 500, 500);
-        this.setLayout(new FlowLayout());
 
         menuBar = new JMenuBar();
 
-        fileMenu = new JMenu("fichier");
-        helpMenu = new JMenu("aide");
-        searchMenu = new JMenu("recherche");
-        productMenu = new JMenu("produit");
+        // Menus
+        fileMenu = new JMenu("Fichier");
+        helpMenu = new JMenu("Aide");
+        searchMenu = new JMenu("Recherche");
+        productMenu = new JMenu("Produit");
 
+        // Menu items
+        exitMenuItem = new JMenuItem("Quitter");
+        fileMenu.add(exitMenuItem);
 
+        userManuelMenuItem = new JMenuItem("Manuel d'utilisation");
+        softwareInfosMenuItem = new JMenuItem("Infos sur l'application");
+        helpMenu.add(userManuelMenuItem);
+        helpMenu.add(softwareInfosMenuItem);
+
+        searchProductMenuItem = new JMenuItem("Un Produit");
+        searchAProductQuantityMenuItem = new JMenuItem("la quantité vendue d’un type de produit dans un délai");
+        searchProductSupplementsDueToEventMenuItem = new JMenuItem("Des produits demandant un réassort supplémentaire");
+        searchAdressMenuItem = new JMenuItem("Les adresses d’une personne/entreprise");
+        searchOutOfStockProductMenuItem = new JMenuItem("Un produit en rupture dans le stock et ses fournisseurs");
+        searchMenu.add(searchProductMenuItem);
+        searchMenu.add(searchAProductQuantityMenuItem);
+        searchMenu.add(searchProductSupplementsDueToEventMenuItem);
+        searchMenu.add(searchAdressMenuItem);
+        searchMenu.add(searchOutOfStockProductMenuItem);
+
+        newProductMenuItem = new JMenuItem("Nouveau");
+        deleteProductMenuItem = new JMenuItem("Suprimer");
+        modifyProductMenuItem = new JMenuItem("Modifier/mettre à jour");
+        findProductMenuItem = new JMenuItem("Trouver");
+        productMenu.add(newProductMenuItem);
+        productMenu.add(deleteProductMenuItem);
+        productMenu.add(modifyProductMenuItem);
+        productMenu.add(findProductMenuItem);
+
+        // welcome
+        welcomePanel = new JPanel(new FlowLayout());
+        welcomeLabel = new JLabel("<html><p>Bienvenue sur le gestionnaire" +
+                "</br>Le manuel d'utilisation se trouve dans le menu aide//Manuel d'utilisation</p></html>");
+        welcomePanel.add(welcomeLabel);
+
+        // window display
+        this.setVisible(true);
     }
 }
