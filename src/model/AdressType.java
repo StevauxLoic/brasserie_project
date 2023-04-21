39e06429ad9842fce48282a6842ce623725194dd
CeceLOursMielleux <etu49139@henallux.be>
2023-04-21 16:30:07 +0200

    commit du labo du 20/04/2023 pour les setters

--- src/model/AdressType.java
+++ src/model/AdressType.java
@@ -1,4 +1,23 @@
 package model;
 
 public class AdressType {
+    private int reference;
+    private String label;
+
+    public AdressType(String label){
+        setReference();
+        setLabel(label);
+    }
+
+    public void setReference() {
+        // recherche dans la base de donnée car c'est un auto implément
+    }
+
+    public void setLabel(String label) {
+        if(label != null){
+            this.label = label;
+        } else {
+            // exeption ?
+        }
+    }
 }
--- src/model/City.java
+++ src/model/City.java
@@ -2,5 +2,11 @@ package model;
 
 public class City {
     private String reference;
+    private String label;
+    private int zipCode;
+    private String countryReference;
 
+    public City(String reference, String label, int zipCode, String countryReference){
+
+    }
 }
