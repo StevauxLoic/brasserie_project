541b88e644eec8d613c7082c0efb7a1bf1ade579
CeceLOursMielleux <etu49139@henallux.be>
2023-04-24 11:07:44 +0200

    commit du 24/04 (continuation des classe de la couche model)

--- src/model/City.java
+++ src/model/City.java
@@ -1,12 +1,15 @@
 package model;
 
 public class City {
-    private String reference;
+    private String reference; // primaryKey
     private String label;
     private int zipCode;
-    private String countryReference;
+    private String countryReference; // foreignKey
 
     public City(String reference, String label, int zipCode, String countryReference){
-
+        this.reference = reference;
+        this.label = label;
+        this.zipCode = zipCode;
+        this.countryReference = countryReference;
     }
 }
--- src/model/Document.java
+++ src/model/Document.java
@@ -1,4 +1,24 @@
 package model;
 
+import java.util.Date;
+
 public class Document {
+    private int reference; // primaryKey
+    private Date date;
+    private Double discount;
+    private Date deadLine;
+    private Boolean isPaid;
+    private Date validityDate;
+    private Boolean isDelivered;
+    private Date deliveryDate;
+    private Double deposit;
+    private Double additionalFees;
+    private int documentTypeReference; // foreignKey
+    private String deliveryAdressReference; // foreignKey
+
+    public Document(int reference, Date date, Double discount, Date deadLine, Boolean isPaid, Date validityDate, Boolean isDelivered, Date deliveryDate, Double deposit, Double additionalFees, int documentTypeReference, String deliveryAdressReference){
+        this.reference = reference;
+
+    }
+
 }
