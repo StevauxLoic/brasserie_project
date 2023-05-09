a3ecdd8666f93a3cbe1e1c2a24080b079dbaaff0
CeceLOursMielleux <etu49139@henallux.be>
2023-05-09 17:49:05 +0200

    crud mis a jour j'avais oublié les min quantity in stock donc j'ai changer la bd aussi (c'est pas encore fini mais on as la base pour rechercher les produit)

--- src/DataAccess/IProductData.java
+++ src/DataAccess/IProductData.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 
 public interface IProductData {
     void createProduct(Product productToCreate) throws SQLException;
-    Product showOneProduct(String referenceOfTheProduct) throws SQLException;
-    ArrayList<Product> showAllProducts() throws SQLException;
+    Product showOneProduct(String referenceOfTheProduct) throws SQLException, ReferenceExeption, TypeExeption, NameExeption, DateExeption;
+    ArrayList<Product> showAllProducts() throws SQLException, ReferenceExeption, TypeExeption, NameExeption, DateExeption;
     void updateProduct(Product productToUpdate) throws SQLException;
     void deleteProduct(Product productToDelete) throws SQLException;
 }
--- src/DataAccess/ProductData.java
+++ src/DataAccess/ProductData.java
@@ -1,9 +1,10 @@
 package DataAccess;
 
-import model.Product;
+import model.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -11,37 +12,66 @@ public class ProductData implements  IProductData{
     private Connection connection = SingletonConnection.getUniqueConnection();
 
     public void createProduct(Product productToCreate) throws SQLException {
-        String sql = "INSERT INTO product (id, type_id, tag, vat, quantity_in_stock, is_sparkling, alcohol_level, launching_date, price, description_of_the_product) " +
-                "VALUES (?,?,?,?,?,?,?,?,?,?)";
+        String sql = "INSERT INTO product (id, type_id, tag, vat, quantity_in_stock, minimum_quantity_in_stock, is_sparkling, alcohol_level, launching_date, price, description_of_the_product) " +
+                "VALUES (?,?,?,?,?,?,?,?,?,?,?)";
         PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1, productToCreate.getReference());
         statement.setInt(2, productToCreate.getTypeReference());
         statement.setString(3, productToCreate.getName());
         statement.setDouble(4, productToCreate.getVat());
         statement.setInt(5, productToCreate.getQuantityInStock());
-        statement.setBoolean(6, productToCreate.isSparkling());
-        statement.setDouble(7, productToCreate.getAlcoholLevel());
-        statement.setDate(8, java.sql.Date.valueOf(productToCreate.getLaunchingDate()));
-        statement.setDouble(9, productToCreate.getPrice());
-        statement.setString(10, productToCreate.getDescription());
+        statement.setInt(6, productToCreate.getMinimumQuantityInStock());
+        statement.setBoolean(7, productToCreate.isSparkling());
+        statement.setDouble(8, productToCreate.getAlcoholLevel());
+        statement.setDate(9, java.sql.Date.valueOf(productToCreate.getLaunchingDate()));
+        statement.setDouble(10, productToCreate.getPrice());
+        statement.setString(11, productToCreate.getDescription());
         statement.executeUpdate();
     }
 
 
-    public Product showOneProduct(String referenceOfTheProduct) throws SQLException{
+    public Product showOneProduct(String referenceOfTheProduct) throws SQLException, ReferenceExeption, TypeExeption, NameExeption, DateExeption {
         String sql = "SELECT id, type_id, tag, vat, quantity_in_stock, is_sparkling, alcohol_level, launching_date, price, description_of_the_product" +
                 "WHERE id = ?";
         PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1, referenceOfTheProduct);
-        statement.executeQuery();
+        ResultSet data = statement.executeQuery();
+        Product product = new Product(data.getString("id"), data.getInt("type_id"), data.getString("tag"), data.getDouble("vat"), data.getInt("minimum_quantity_in_stock"),
+                data.getBoolean("is_sparkling"), data.getDate("launching_date").toLocalDate(), data.getDouble("price"), data.getDouble("alcohol_level"), data.getString("description_of_the_product"),
+                data.getInt("quantity_in_stock")
+        ) ;
+        return product;
     }
 
-    public ArrayList<Product> showAllProducts()throws  SQLException{
+    public ArrayList<Product> showAllProducts()throws SQLException, ReferenceExeption , TypeExeption, NameExeption, DateExeption{
+        ArrayList<Product> products = new ArrayList<>();
+        Product product;
+        String sql = "SELECT * FROM Product";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        try {
+            ResultSet data = statement.executeQuery();
+            String VATNumber, siteName;
 
+            while (data.next()) {
+                 product = new Product(data.getString("id"), data.getInt("type_id"), data.getString("tag"), data.getDouble("vat"), data.getInt("minimum_quantity_in_stock"),
+                        data.getBoolean("is_sparkling"), data.getDate("launching_date").toLocalDate(), data.getDouble("price"), data.getDouble("alcohol_level"), data.getString("description_of_the_product"),
+                        data.getInt("quantity_in_stock")
+                ) ;
+
+
+
+                products.add(product);
+            }
+
+        }catch (SQLException e) {
+
+        }
+
+        return products;
     }
 
     public void updateProduct(Product productToUpdate) throws SQLException{
-        String sql = "UPDATE product set (id = ?, type_id = ?, tag = ?, vat = ?, quantity_in_stock = ?, is_sparkling = ?, alcohol_level = ?, launching_date = ?, price = ?, description_of_the_product = ?) " +
+        String sql = "UPDATE product set (id = ?, type_id = ?, tag = ?, vat = ?, quantity_in_stock = ?, minimum_quantity_in_stock = ?, is_sparkling = ?, alcohol_level = ?, launching_date = ?, price = ?, description_of_the_product = ?)" +
                 "WHERE id = ?";
         PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1, productToUpdate.getReference());
@@ -49,12 +79,13 @@ public class ProductData implements  IProductData{
         statement.setString(3, productToUpdate.getName());
         statement.setDouble(4, productToUpdate.getVat());
         statement.setInt(5, productToUpdate.getQuantityInStock());
-        statement.setBoolean(6, productToUpdate.isSparkling());
-        statement.setDouble(7, productToUpdate.getAlcoholLevel());
-        statement.setDate(8, java.sql.Date.valueOf(productToUpdate.getLaunchingDate()));
-        statement.setDouble(9, productToUpdate.getPrice());
-        statement.setString(10, productToUpdate.getDescription());
-        statement.setString(11, productToUpdate.getReference());
+        statement.setInt(6, productToUpdate.getMinimumQuantityInStock());
+        statement.setBoolean(7, productToUpdate.isSparkling());
+        statement.setDouble(8, productToUpdate.getAlcoholLevel());
+        statement.setDate(9, java.sql.Date.valueOf(productToUpdate.getLaunchingDate()));
+        statement.setDouble(10, productToUpdate.getPrice());
+        statement.setString(11, productToUpdate.getDescription());
+        statement.setString(12, productToUpdate.getReference());
         statement.executeUpdate();
     }
 
