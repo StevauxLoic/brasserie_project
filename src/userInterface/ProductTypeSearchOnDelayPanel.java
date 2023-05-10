5d20164a1142e6f16476eab678ed4ab4c1e9c193
StevauxLoic <etu48693@henallux.be>
2023-05-10 17:10:49 +0200

    avancemen sur les recherche (productype on a delay)

--- src/userInterface/ProductTypeSearchOnDelayPanel.java
+++ src/userInterface/ProductTypeSearchOnDelayPanel.java
@@ -1,6 +1,139 @@
 package userInterface;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ProductTypeSearchOnDelayPanel extends JPanel {
+    private JComboBox productTypeComboBox;
+    private JSpinner delayBeginingSpinner,
+                        delayEndSpinner;
+    private JLabel titleLabel,
+                    productTypeLabel,
+                    delayBeginingLabel,
+                    delayEndLabel;
+    private JPanel titlePanel,
+                    formPanel,
+                    buttonsPanel;
+    private JButton searchButton;
+    private Date delayBeginingDateSelected,
+                    delayEndDateSelected;
+
+    public ProductTypeSearchOnDelayPanel() {
+        this.setLayout(new BorderLayout());
+
+        // panels
+        titlePanel = new JPanel();
+        titlePanel.setLayout(new FlowLayout());
+
+        formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(3,2));
+
+        buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new FlowLayout());
+
+        // listener
+        ButtonListener buttonListener = new ButtonListener();
+
+        // modules
+        titleLabel = new JLabel("Recherche de produit vendu dans un certain délai");
+        titlePanel.add(titleLabel);
+
+
+        productTypeLabel = new JLabel("type de produit");
+        // TODO get the types list
+        productTypeComboBox = new JComboBox(new String[]{"spiritueu", "bière", "soda", "whisky"});
+        formPanel.add(productTypeLabel);
+        formPanel.add(productTypeComboBox);
+
+        delayBeginingLabel = new JLabel("date de début");
+        delayBeginingDateSelected = new Date();
+        delayBeginingSpinner = new JSpinner(new SpinnerDateModel(delayBeginingDateSelected, null, ((Date) delayEndSpinner.getValue()), Calendar.YEAR)); // TODO verif le début et fin des dates selectable
+        JSpinner.DateEditor delayBeginingEditor = new JSpinner.DateEditor(delayBeginingSpinner,"dd/MM/yyyy");
+        delayBeginingSpinner.setEditor(delayBeginingEditor);
+        formPanel.add(delayBeginingLabel);
+        formPanel.add(delayBeginingSpinner);
+
+        delayEndLabel = new JLabel("date de fin");
+        delayEndSpinner = new JSpinner();
+        delayEndDateSelected = new Date();
+        delayEndSpinner = new JSpinner(new SpinnerDateModel(delayEndDateSelected, ((Date) delayBeginingSpinner.getValue()), null, Calendar.YEAR)); // TODO verif le début et fin des dates selectable
+        JSpinner.DateEditor delayEndEditor = new JSpinner.DateEditor(delayEndSpinner,"dd/MM/yyyy");
+        delayEndSpinner.setEditor(delayEndEditor);
+        formPanel.add(delayEndLabel);
+        formPanel.add(delayEndSpinner);
+
+
+        searchButton = new JButton("chercher");
+        searchButton.addActionListener(buttonListener);
+        buttonsPanel.add(searchButton);
+
+        // fill the panel and display it
+        this.add(titlePanel, BorderLayout.NORTH);
+        this.add(formPanel, BorderLayout.CENTER);
+        this.add(buttonsPanel, BorderLayout.SOUTH);
+
+        this.setVisible(true);
+    }
+
+    public LocalDate getDelayBeginingDate() {
+        return ((Date) delayBeginingSpinner.getValue()).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    public LocalDate getDelayEndDate() {
+        return ((Date) delayEndSpinner.getValue()).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    public boolean isFormValid() {
+        if (productTypeComboBox.getSelectedItem() == null) {
+            JOptionPane.showMessageDialog(null, "un type d'objet doit être sélectionné", "erreur de type de produit", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        LocalDate delaybeginingDate = getDelayBeginingDate();
+        LocalDate endingDate = getDelayEndDate();
+
+        if (delaybeginingDate.equals(endingDate)) {
+            JOptionPane.showMessageDialog(null, "les deux dates (début et fin) ne peuvent être identique", "erreur de dates", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (delaybeginingDate.isAfter(endingDate)) {
+            JOptionPane.showMessageDialog(null, "la date de début doit être avant la date de fin", "erreur de dates", JOptionPane.ERROR_MESSAGE);
+        }
+        return true;
+    }
+
+    private void search() {
+        if (isFormValid()) {
+            // TODO recherche et afficher dans le JTable
+            ArrayList<ProductSoldInADelay> productTypesSoldList = new ArrayList<ProductSoldInADelay>();
+            if (!productTypesSoldList.isEmpty()) {
+                this.removeAll();
+
+                AllProductSoldInADelayModel allProductSoldInADelayModel = new AllProductSoldInADelayModel(productTypesSoldList);
+                JTable productsTable = new JTable(allProductSoldInADelayModel);
+                productsTable.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+                ListSelectionModel listSelect = productsTable.getSelectionModel();
+
+            } else {
+                JOptionPane.showMessageDialog(null, "aucunes vente n'a été trouvée dans ce délai", "aucune donnée trouvée", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
+    }
+
+    private class ButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            ProductTypeSearchOnDelayPanel.this.search();
+        }
+    }
+
 }
