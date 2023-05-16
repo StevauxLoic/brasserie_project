847879949ac0052b13ce3bbadac815eea751bafc
CeceLOursMielleux <etu49139@henallux.be>
2023-05-16 11:07:32 +0200

    ajout des 2 requete sql

--- src/dataAccess/IProductData.java
+++ src/dataAccess/IProductData.java
@@ -10,4 +10,6 @@ public interface IProductData {
     ArrayList<Product> getAllProducts() throws GetDatasException, CreateConnectionException;
     void updateProduct(Product productToUpdate) throws ModifyDatasException, CreateConnectionException;
     void deleteProduct(Product productToDelete) throws DeleteDatasException,CreateConnectionException;
+    boolean productHasLinksWithOthersDatas(Product product) throws GetDatasException, CreateConnectionException; // return true if links exist
+    boolean productIdAlreayExist(Product productToVerify) throws GetDatasException, CreateConnectionException; // return true if id already exist
 }
